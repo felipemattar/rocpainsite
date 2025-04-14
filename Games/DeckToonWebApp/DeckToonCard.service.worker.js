ddbe13a27c17f771a01452efade4ca0cff62a053
Felipe Mattar <fdesigner@gmail.com>
2025-04-14 03:25:08 -0300

    up

--- Games/DeckToonWebApp/DeckToonCard.service.worker.js
+++ Games/DeckToonWebApp/DeckToonCard.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1744611047|52933308126';
+const CACHE_VERSION = '1744611899|53785758223';
 /** @type {string} */
 const CACHE_PREFIX = 'DeckToonCard-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
